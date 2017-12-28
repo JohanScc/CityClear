baf4b04892535442ce0a9d9a8a52cd646ec4f208
johanscc.1912@gmail.com <johanscc.1912@gmail.com>
2017-12-28 11:41:17 -0500

    Index.html

--- server.js
+++ server.js
@@ -38,6 +38,32 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+/// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 app.listen(port);
 
 app.use(function(req, res) {
