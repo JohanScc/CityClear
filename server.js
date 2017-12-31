70d9f14fce176cffc27d23d5f97c5fd207d3948b
johanscc.1912@gmail.com <johanscc.1912@gmail.com>
2017-12-30 21:25:31 -0500

    CORS

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/routes'); //importing route
 routes(app); //register the route
 app.use(express.static(__dirname + '/public'));
+
+
+app.all("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  return next();
+});
 /*
 app.use(function (req, res, next) {
 
